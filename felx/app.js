a539a339fc6458c88f32bf536010f67f95c53bfa
Arham Mirza <arhammirza927@gmail.com>
2022-09-27 17:55:45 +0500

    upgradefolder

---
+++ felx/app.js
@@ -0,0 +1,14 @@
+// var jani ="bro";
+// alert("hellow " + jani)
+
+// var value = "kia ho rha ";
+// alert(value + "hai");
+
+// var change="what is your ";
+// alert(change + "name");
+
+
+function abc(a){
+    document.getElementById("ab").src=a; 
+
+}
---
+++ javascript/app.js
@@ -0,0 +1,64 @@
+// var input = parseInt(prompt("enter your email")) ;
+// var numeric =parseInt(prompt("add a number"));
+// var input = parseFloat(prompt("enter your email")) ;
+// var numeric = parseFloat(prompt("add a number"));
+// var add = input+numeric;
+// document.write(add);   
+ 
+
+
+
+// var num = 12;
+
+// var loan = 100000;
+// var interest = 8;
+// var amount =( loan*interest) / 100; 
+// document.write(amount);
+
+// var a = 10++;
+
+// document.write(a);
+
+// var name = 10;
+// var name1 = 20;
+// var name2 = 30;
+
+
+
+// document.write("<br>" + name++);
+// document.write("<br>" + ++name);
+// document.write("<br>" + name++);
+
+// var num = 12;
+// ++num;
+// ++num;
+// num--;
+// ++num;
+// num--;
+// var num1 = 5;
+
+
+
+
+// document.write(num + num1);
+
+// var a = 5;
+// var b = 6;
+// var c =7;
+// document.write(++a  + "<br>");
+// document.write(a++);
+
+// logical operator
+// var num = "john";
+// var num1 = 22;
+// document.write(num=="john" && age==22);
+// document.write(num=="ali" && age==22);
+// document.write(num=="ali" || age==22);
+
+
+var num = "john";
+var num1 = 22; 
+
+document.write(num!="Ali" && age!= 22);
+// document.write(num!="Ali" && age!= 22);
+// document.write(num!="john" || age!= 22);
